70f655238247bd3c3f564d053db57fc04150724f
devl333 <superphs333@naver.com>
2026-01-24 21:28:29 +0900

    tip테이블관련

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tips()
+    {
+        return $this->hasMany(Tip::class);
+    }
 }
