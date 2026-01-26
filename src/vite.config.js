4330250ef1fccc333d055e8b12e839cd77a18d60
devl333 <superphs333@naver.com>
2026-01-26 15:38:15 +0900

    breeze

--- src/vite.config.js
+++ src/vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: { host: 'localhost', clientPort: 5174 },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
